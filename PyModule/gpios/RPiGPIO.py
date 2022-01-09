e05e751b4431e4cc63a93a3f8e67b3346a710bda
Kenvix Zure <kenvixzure@live.com>
2022-01-09 18:01:15 +0800

    Door Control (3)

--- PyModule/gpios/RPiGPIO.py
+++ PyModule/gpios/RPiGPIO.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from RPi import GPIO
 import asyncio
 
@@ -6,15 +8,22 @@ if os.getenv("LOCKER_GPIO_DRIVER").lower() == "fake-verbose":
     GPIO.VERBOSE = True
 
 IsDoorOperating = False
+IsFakeGPIO = os.getenv("LOCKER_GPIO_DRIVER").lower().startswith("fake")
+WaitTimeUserDoor = float(os.getenv("LOCKER_GPIO_DOOR_WAIT_USER_DELAY"))
+PinBeep = int(os.getenv("LOCKER_GPIO_PIN_BEEP"))
+
 
 if os.getenv("LOCKER_GPIO_DOOR_DRIVER").lower() == "sg996r":
-    import SG996R as door
+    import Steering as door
 else:
     import DirectDoor as door
 
 
-async def door_init():
+async def init():
+    GPIO.setmode(GPIO.BOARD)
     await door.door_init()
+    GPIO.setup(PinBeep, GPIO.OUT)
+    GPIO.output(PinBeep, GPIO.LOW)
 
 
 async def door_open():
@@ -26,7 +35,23 @@ async def door_close():
 
 
 async def door_unlock():
+    global IsDoorOperating
+    if IsDoorOperating:
+        return
+
+    IsDoorOperating = True
     await door_open()
-    await asyncio.sleep(int(os.getenv("LOCKER_GPIO_DOOR_WAIT_USER_DELAY")))
+    await asyncio.sleep(WaitTimeUserDoor)
     await door_close()
+    IsDoorOperating = False
     pass
+
+
+async def beep(voiceDelay=0.3, silentDelay=0.25, times=1):
+    for i in range(0, times):
+        GPIO.output(PinBeep, GPIO.HIGH)
+        await asyncio.sleep(voiceDelay)
+        GPIO.output(PinBeep, GPIO.LOW)
+        await asyncio.sleep(silentDelay)
+        # Also beep user Terminal!
+        print('\a', file=sys.stderr)
